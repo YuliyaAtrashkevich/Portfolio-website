2be81f41ec4c2c1c3cfbd5d38bcfff8d5985bc6e
YuliyaAtrashkevich <trafimava@gmail.com>
2023-07-30 23:10:19 +0600

    Анимация портфолио, всплывающее меню

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var portfolio = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(portfolio);
+    var burger = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(burger);
+  });
